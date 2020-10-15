ac8c0baec61670b6728e5270f19d0580d00853d3
aellis14 <adriana.ellis@patelhs.org>
2020-10-15 22:12:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+       boolean sunny = true;
+       boolean hot = false;
+       if (sunny)
+         if (hot)
+             System.out.println("Head for the beach!");
+        else // Which if is else attached to??
+       System.out.println("Bring a towel!");
+
+       System.out.println("how many people are comming?");
+
+        for(int count = 1; count <= 5; count++)
+     
+        System.out.println(count);
+          }
+   }
+
